589c88bea0c17a20cb208f690f096d9486920c59
484021 <bhoopalsanthosh@gmail.com>
2024-09-28 23:56:32 -0400

    feat: type error

--- components/top-airing-anime.tsx
+++ components/top-airing-anime.tsx
@@ -2,15 +2,7 @@
 import { CONSUMET_API_URL } from "@/lib/constants";
 import { Data, EpisodeResult } from "@/lib/types";
 import React, { useEffect, useState } from "react";
-import {
-  Card,
-  CardContent,
-  //   CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
-import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -41,11 +33,8 @@ export default function TopAiringAnime() {
       <div className="pt-2 flex flex-wrap w-full h-full">
         <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-10 ">
           {results.map((result) => (
-            <Link href={`/anime/${result.id}`}>
-              <Card
-                className="w-[140px] m-2 flex flex-col items-center justify-center hover:scale-110 transition-all dark:shadow-purple-950"
-                key={result.id}
-              >
+            <Link href={`/anime/${result.id}`} key={result.id}>
+              <Card className="w-[140px] m-2 flex flex-col items-center justify-center hover:scale-110 transition-all dark:shadow-purple-950">
                 <CardHeader className="text-center">
                   {/* <CardDescription className="text-lg">{index + 1}</CardDescription> */}
                   <CardTitle>{result.title}</CardTitle>
