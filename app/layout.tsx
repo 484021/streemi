ec8845c1cdd797cce28f7db65fd2a8b0e678bad6
484021 <bhoopalsanthosh@gmail.com>
2024-10-09 19:29:19 -0400

    feat: tag manager fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./styles/globals.css";
@@ -54,7 +54,7 @@ export default function RootLayout({
         <Footer />
         <Analytics />
         <Toaster />
-        <GoogleTagManager gtmId="G-WK7FQR2HL1" />
+        <GoogleAnalytics gaId="G-Y3Y1HDC9Q5" />
       </body>
     </html>
   );
